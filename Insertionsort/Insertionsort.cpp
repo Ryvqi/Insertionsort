093864a3f605618267484aa2bb1eced3ca7026d3
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-24 11:09:31 +0700

    membuar fungsi

--- Insertionsort/Insertionsort.cpp
+++ Insertionsort/Insertionsort.cpp
@@ -58,21 +58,24 @@ void insertionsort() {
 		}
 
 	}
-
-
-	void display() {
-		cout << endl;
-		cout << "\n================================" << endl;
-		cout << "Element Array yang telah tersusun" << endl;
-		cout << "=================================" << endl;
-
-		for (int j = 0; j < n; j++) {
-			cout << arr[j] << endl;
-		}
-		cout << endl;
 }
+
+void display() {
+	cout << endl;
+	cout << "\n================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
 }
+
 int main()
 {
+	input();
+	insertionsort();
+	display();
 
 }
