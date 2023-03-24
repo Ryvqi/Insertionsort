4d06adbc077576a734d36b0744ef6a23a37456de
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-24 10:35:37 +0700

    membuat data

--- Insertionsort/Insertionsort.cpp
+++ Insertionsort/Insertionsort.cpp
@@ -22,6 +22,12 @@ void input() {
 	cout << "=====================" << endl;
 	cout << "Masukan Element Array" << endl;
 	cout << "=====================" << endl;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+	}
 }
 int main()
 {
